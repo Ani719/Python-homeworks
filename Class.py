75d66df1f31993456fd911b404d9436bcff41517
Ani719 <57664422+Ani719@users.noreply.github.com>
2020-01-16 18:42:50 +0400

    Class

---
+++ Class.py
@@ -0,0 +1,36 @@
+class Examination:
+    def __init__(self):
+        self.candidates = candidates
+        self.exam_officers = exam_officers
+        self.course_ID = course_ID
+        self.date = date
+        self.location = location
+        self.questions = questions
+        self.number_of_questions = number_of_questions
+        self.points = points
+        self.answers = answers
+        self.pass_rates= pass_rates
+        self.exam_length = exam_length
+
+    def take_en_exam(self):
+        self.candidates = candidates
+
+    def getMarks(self):
+        self.candidates = candidates
+
+    def createDate(self):
+        self.date = date
+
+    def setLocation(self):
+        self.location = location
+
+    def checkResult(self):
+        self.answers = answers
+
+    def countPoints(self)
+        self.points = points:
+
+candidates = []
+
+
+
